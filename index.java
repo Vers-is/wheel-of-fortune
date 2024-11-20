9a464135935d32b195a9f39b6f8726fd211e3a00
Ver_s <aleksandra.bystrova@alatoo.edu.kg>
2024-11-20 09:53:39 +0600

    fixing, update

--- index.java
+++ index.java
@@ -28,7 +28,7 @@ public class index {
 
     public static void awareRules(Scanner scanner) {
         cleanConsole();
-        System.out.print("\033[96m| \033[1mRULES<<<<<<<<<<<<<<<<<<<<\033[0m\n\033[94m| Max score: 500 points\n| 1 letter = 10 points\n| 1 word = 50 points\n| 10 words to guess\n+-------------------------\n\033[0m| Enter anything if got it: ");
+        System.out.print("\033[96m| \033[1mRULES<<<<<<<<<<<<<<<<<<<<\033[0m\n\033[94m| Max score: 600 points\n| 1 letter = 10 points\n| 1 word = 50 points\n| 10 words to guess\n+-------------------------\n\033[0m| Enter anything if got it: ");
         String input = scanner.nextLine().trim();
     }
 
@@ -57,25 +57,17 @@ public class index {
     public static HashMap<String, String> saveWords() {
         HashMap<String, String> hints = new HashMap<>();
 
-        hints.put("CAT", "Pet with whiskers\n" +
-                "| and fluffy ears.");
-        hints.put("BREAD", "A baked staple food made\n" +
-                "| from flour and water.");
-        hints.put("RABBIT", "A small mammal with long ears\n" +
-                "| and strong hind legs.");
+        hints.put("CAT", "Pet with whiskers\n| and fluffy ears.");
+        hints.put("BREAD", "A baked staple food made\n| from flour and water.");
+        hints.put("RABBIT", "A small mammal with long ears\n| and strong hind legs.");
         hints.put("MOTHER", "A female parent.");
-        hints.put("HAIR", "Grow from the skin,\n" +
-                "| mainly on the head.");
+        hints.put("HAIR", "Grow from the skin,\n| mainly on the head.");
         hints.put("JACKET", "Cloth worn for warmth.");
         hints.put("EYE", "The organ for vision.");
-        hints.put("LIGHTENING", "A bright electrical\n" +
-                "| discharge seen during storms.");
-        hints.put("LEG", "A part of body used\n" +
-                "| for standing and walking.");
-        hints.put("INFINITY", "A concept representing\n" +
-                "| endlessness.");
-        hints.put("LIMIT", "A boundary or point beyond which\n" +
-                "| something does not extend.");
+        hints.put("LIGHTENING", "A bright electrical\n| discharge seen during storms.");
+        hints.put("LEG", "A part of body used\n| for standing and walking.");
+        hints.put("INFINITY", "A concept representing\n| endlessness.");
+        hints.put("LIMIT", "A boundary or point beyond which\n| something does not extend.");
         return hints;
     }
 
@@ -110,89 +102,118 @@ public class index {
 
     public static void checkAnswer(String name) {
         int score = scores.get(name);
+        String message = playerMessages.getOrDefault(name, "No recent messages.");
         System.out.println("\u001B[33m>>> " + name + "'s score: " + score + "\u001B[0m");
+    }
+
+    public static boolean allLettersGuessed(String word, ArrayList<Character> guessedLetters) {
+        for (char c : word.toCharArray()) {
+            if (!guessedLetters.contains(c)) {
+                return false; // ???? ???? ?? ???? ????? ?? ???????
+            }
+        }
+        return true;
     }
 
     public static void playerGuess(ArrayList<String> players, Scanner scanner) {
         int playerIndex = 0;
         ArrayList<String> wordList = saveWordsList();
-        int wordIndex = 0;
         ArrayList<Character> guessedLetters = new ArrayList<>();
+        boolean gameEnded = false;
     
         for (String player : players) {
             scores.put(player, 0);
             playerMessages.put(player, "");
         }
     
-        while (true) {
-            if (wordIndex >= wordList.size()) {
-                cleanConsole();
-                System.out.println("\u001B[35mAll words have been guessed!");
-                System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>\u001B[0m");
-                System.out.println("\033[96m| FINAL SCORES:\n+-------------------------");
-                for (String player : players) {
-                    System.out.println("\033[96m| " + player + ":\u001B[0m " + scores.get(player) + " points");
+        while (!gameEnded) {
+            for (String currentWord : wordList) {
+                guessedLetters.clear();
+                boolean wordCompleted = false;
+    
+                while (!wordCompleted) {
+                    cleanConsole();
+                    String currentPlayer = players.get(playerIndex);
+                    printWordHint(currentWord, guessedLetters);
+                    checkAnswer(currentPlayer);
+    
+                    String message = playerMessages.get(currentPlayer);
+                    if (!message.isEmpty()) {
+                        System.out.println("\u001B[36m>>> " + message + "\u001B[0m");
+                        playerMessages.put(currentPlayer, ""); 
+                    }
+    
+                    System.out.print("\u001B[32m" + currentPlayer + "'s turn! Guess: \u001B[0m");
+                    String guess = scanner.nextLine().trim().toUpperCase();
+    
+                    if (guess.isEmpty() || (!guess.matches("[A-Z]+") && !guess.equalsIgnoreCase(currentWord))) {
+                        System.out.println("\u001B[31mInvalid input!\u001B[0m");
+                        playerMessages.put(currentPlayer, "Invalid input.");
+                        continue;
+                    }
+    
+                    boolean correct = false;
+                    if (guess.length() == 1) {
+                        char guessedChar = guess.charAt(0);
+                        if (guessedLetters.contains(guessedChar)) {
+                            playerMessages.put(currentPlayer, "Repeated letter.");
+                        } else if (currentWord.indexOf(guessedChar) >= 0) {
+                            guessedLetters.add(guessedChar);
+                            scores.put(currentPlayer, scores.get(currentPlayer) + 10);
+                            playerMessages.put(currentPlayer, "Correct! You earned the turn.");
+                            correct = true;
+    
+                            if (allLettersGuessed(currentWord, guessedLetters)) {
+                                scores.put(currentPlayer, scores.get(currentPlayer) + 50);
+                                cleanConsole();
+                                System.out.println("\u001B[33mCongrats! \"" + currentWord + "\" has been guessed!\u001B[0m");
+                                playerMessages.put(currentPlayer, "Next word!");
+                                wordCompleted = true;
+                            }
+                        } else {
+                            playerMessages.put(currentPlayer, "Wrong.");
+                        }
+                    } else if (guess.equals(currentWord)) {
+                        scores.put(currentPlayer, scores.get(currentPlayer) + 50);
+                        cleanConsole();
+                        System.out.println("\u001B[33mCongrats! " + currentPlayer + " guessed the word \"" + currentWord + "\"!\u001B[0m");
+                        playerMessages.put(currentPlayer, "Next word!");
+                        wordCompleted = true;
+                    } else {
+                        playerMessages.put(currentPlayer, "Wrong.");
+                    }
+    
+                    if (!correct && !wordCompleted) {
+                        playerIndex = (playerIndex + 1) % players.size();
+                    }
                 }
-                break;
-            }
-            String currentPlayer = players.get(playerIndex);
-            String currentWord = wordList.get(wordIndex);
-            printWordHint(currentWord, guessedLetters);
     
-            checkAnswer(currentPlayer);
-    
-            boolean wordCompleted = true;
-            for (char c : currentWord.toCharArray()) {
-                if (!guessedLetters.contains(Character.toUpperCase(c))) {
-                    wordCompleted = false;
+                System.out.print("\u001B[34mDo you want to continue to the next word? (yes/no): \u001B[0m");
+                String continueGame = scanner.nextLine().trim().toLowerCase();
+                if (continueGame.equals("no")) {
+                    gameEnded = true;
                     break;
                 }
             }
     
-            if (wordCompleted) {
+            if (!gameEnded) {
                 cleanConsole();
-                System.out.println("\u001B[32mThe word \"" + currentWord + "\" has been revealed!\u001B[0m");
-                scores.put(currentPlayer, scores.get(currentPlayer) + 10);
-                wordIndex++;
-                guessedLetters.clear();
-                continue;
-            }
-    
-            System.out.print("\u001B[32m" + currentPlayer + "'s turn! Guess: ");
-            String guess = scanner.nextLine().trim().toUpperCase();
-    
-            if (guess.isEmpty()) {
-                System.out.println("\u001B[31mInvalid input! Please enter a letter or a word.\u001B[0m");
-                continue;
-            }
-    
-            String message;
-            
-            cleanConsole();
-            
-            if (guess.length() == 1) {
-                char guessedChar = guess.charAt(0);
-                if (guessedLetters.contains(guessedChar)) {
-                    message = "\u001B[31mThis letter has already been guessed!\u001B[0m";
-                } else if (currentWord.indexOf(guessedChar) >= 0) {
-                    guessedLetters.add(guessedChar);
-                    scores.put(currentPlayer, scores.get(currentPlayer) + 10);
-                    message = "\u001B[32mCorrect! The letter is in the word.\u001B[0m";
-                } else {
-                    message = "\u001B[31mIncorrect! The letter is not in the word.\u001B[0m";
+                System.out.println("\033[96m| FINAL SCORES:\n+-------------------");
+                for (String player : players) {
+                    System.out.println("\033[96m| " + player + ":\u001B[0m " + scores.get(player) + " points");
                 }
-            } else if (guess.equals(currentWord)) {
-                message = "\u001B[32mCongrats! " + currentPlayer + " guessed the word \"" + currentWord + "\"!\u001B[0m";
-                scores.put(currentPlayer, scores.get(currentPlayer) + 50);
-                wordIndex++;
-                guessedLetters.clear();
-            } else {
-                message = "\u001B[31mIncorrect! The guess is not the word.\u001B[0m";
+                System.out.println("\u001B[33mAll words have been guessed!\u001B[0m");
+                gameEnded = true;
+            } else if (gameEnded) {
+                cleanConsole();
+                System.out.println("\033[96m| FINAL SCORES:\n+------------------");
+                for (String player : players) {
+                    System.out.println("\033[96m| " + player + ":\u001B[0m " + scores.get(player) + " points");
+                    
+                }
+                
+                gameEnded = true;
             }
-    
-            System.out.println("\n" + message);
-    
-            playerIndex = (playerIndex + 1) % players.size();
         }
     }
     
